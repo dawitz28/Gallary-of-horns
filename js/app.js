39ba76e7b81bb98e3375f5e67fe5d74bfb7217c0
Michael Campbell <cam3000@gmail.com>
2020-12-15 21:07:56 -0800

    pagination is now working and readme is up to date

--- js/app.js
+++ js/app.js
@@ -4,13 +4,10 @@ let hornArray = [];
 let keyword = [];
 let $page1 = $('#page1');
 let $page2 = $('#page2');
-let $container = $('photo-container');
+let $container = $('.photo-container');
 //let $template = $('#photo-template').html;
 //let$dropdown = $('#dropdown');
-// let $dropdown2 = $ $('#dropdown2');
-//let $page1 = $('#page1');
-// let $page2 = $('#page2');
-//let photoArray = [];
+let $dropdown2 = $('#dropdown2');
 let showPhotos = [];
 
 function HornInfo(obj) {
@@ -30,23 +27,27 @@ if (this.url === './data/page-1.json') {
   page = 2;
 }
 
+function renderHornArray() {
+  console.log(hornArray);
+  hornArray.forEach($HornTemplate => {
+    let $newHorn = $('#photo_template').clone();
+    $newHorn.removeAttr('id');
+    $newHorn.find('h2').text($HornTemplate.title);
+    let hornPath = $HornTemplate.imgUrl;
+    $newHorn.find('img').attr('src', hornPath);
+    $newHorn.find('p').text($HornTemplate.description);
+    $newHorn.addClass($HornTemplate.keyword);
+    $('main').append($newHorn);
+    console.log($HornTemplate);
+
+    if (!keyword.includes($HornTemplate.keyword)) keyword.push($HornTemplate.keyword);
+  });
+}
 
 $.ajax('./data/page-1.json')
   .then(data => {
     data.forEach(object => new HornInfo(object));
-    hornArray.forEach($HornTemplate => {
-      let $newHorn = $('#photo_template').clone();
-      $newHorn.removeAttr('id');
-      $newHorn.find('h2').text($HornTemplate.title);
-      let hornPath = $HornTemplate.imgUrl;
-      $newHorn.find('img').attr('src', hornPath);
-      $newHorn.find('p').text($HornTemplate.description);
-      $newHorn.addClass($HornTemplate.keyword);
-      $('main').append($newHorn);
-      console.log($HornTemplate);
-
-      if (!keyword.includes($HornTemplate.keyword)) keyword.push($HornTemplate.keyword);
-    });
+    renderHornArray();
     console.log(keyword);
     keyword.forEach(key => {
       let newoption = $(`<option>${key}</option>`);
@@ -57,20 +58,43 @@ $.ajax('./data/page-1.json')
       let value = $(this).val();
       $('section').hide();
       $(`.${value}`).show();
-      if (value === 'default') $('section').show();
+      if (value === 'default') $('main').find('section').show();
     });
-    // pagination implementing
+    //event handler for keyword dropdown
+    $dropdown2.change(function () {
+      let $photos = $('.photo');
+      let val = $(this).val();
+      let $photoToShow = $('.' + val);
+      if (val === 'default') {
+        $photos.show();
+      } else {
+        $photos.hide();
+        $photoToShow.show();
+      }
+    });
+
+    // pagination implementation
     $page1.click(function () {
       $container.empty();
       hornArray = [];
-      $.ajax('./data/page-1.json').then(showPhotos);
+      $.ajax('./data/page-1.json').then(data => {
+        data.forEach(object => new HornInfo(object));
+        renderHornArray();
+      });
     });
 
     $page2.click(function () {
       $container.empty();
       hornArray = [];
-      $.ajax('./data/page-2.json').then(showPhotos);
+      $.ajax('./data/page-2.json').then(data => {
+        data.forEach(object => new HornInfo(object));
+        renderHornArray();
+      });
     });
+    // handler for the sort dropdown
+    $dropdown2.change(function () {
+      let val = $(this).val();
 
+    });
 
   });
